aa8f67ed339bbebb1a3a25d4caa6efe9f0c36c54
burhanshaikh <burhanshaikh87@gmail.com>
2020-11-08 14:30:05 +0530

    modified server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const express = require('express');
+const exp = require('express');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
+const app = exp();
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
